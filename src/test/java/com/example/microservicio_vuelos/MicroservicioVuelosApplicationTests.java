5c78f75f01ab58900fc44f0e3116a507b0e4c3c9
edwinnsilva <edwinn.silvalamus@gmail.com>
2021-08-11 15:00:38 -0500

    Initial commit

---
+++ src/test/java/com/example/microservicio_vuelos/MicroservicioVuelosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicio_vuelos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioVuelosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
